08f3866038ac20173c296498a15eab9777c28d94
VicenteGO <vicentejose.guigooltra@cieep.com>
2024-05-13 01:28:52 +0200

    catalogo

--- veats/src/app/catalogo/catalogo.component.ts
+++ veats/src/app/catalogo/catalogo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink, RouterLinkActive],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
